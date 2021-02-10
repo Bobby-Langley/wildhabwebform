b5bb3ac9634490e35be64b89483a0d9c2587e3d1
Bobby Langley <bobbylangley227@gmail.com>
2021-02-10 11:57:11 -0500

    added a list and a favorites

--- src/ListEvents/index.jsx
+++ src/ListEvents/index.jsx
@@ -1,34 +1,46 @@
+import { Col, Row, List } from "antd";
+
 import React, { useState, useEffect } from "react";
 
+const addToFavorites = (id) => {
+    console.log('button clicked', id)
+}
+
 const ListEvents = () => {
-    const [eventsList, setEventsList] = useState([])
-    useEffect(() => {
-        //api call GetEvents
-        fetch("https://wildhab-api-a.web.app/events")
+  const [eventsList, setEventsList] = useState([]);
+  useEffect(() => {
+    //api call GetEvents
+    fetch("https://wildhab-api-a.web.app/events")
       .then((result) => result.json())
-      .then(data => setEventsList(data.data))
-      .catch((error) => console.log("error", error))
-    }, [] )
+      .then((data) => setEventsList(data.data))
+      .catch((error) => console.log("error", error));
+  }, []);
 
-    //store events in state
-    //display events
-
-    /*
-        eventName: '',
-        sport: '',
-        eventDuration,
-
-    */
-
-    return(
-        <>
-        <div>Hello List Events</div>
-    {eventsList && eventsList.map(event => 
-        <p>{event.eventName || event.name}, {event.sport}, Duration: {event.eventDuration} </p> 
-        )}
+  return (
+    <>
+      <Row justify="space-around">
+        <Col xs={24} sm={10}>
+          <h1 style={{ textAlign: "center" }}>Hello List Events</h1>
+        </Col>
+      </Row>
+      <Row justify="space-around">
+        <Col>
+          <List
+            dataSource={eventsList}
+            renderItem={(event) => (
+              <List.Item
+                key={event.id}
+                actions={[<a key="add-to-favorites" onClick={addToFavorites.bind(undefined, event.id)}>Add to favorites</a>]}
+              >
+                {event.eventName || event.name} {event.sport} Duration:
+                {event.eventDuration}
+              </List.Item>
+            )}
+          />
+        </Col>
+      </Row>
     </>
-    )}
+  );
+};
 
-
-
-export default ListEvents
+export default ListEvents;
